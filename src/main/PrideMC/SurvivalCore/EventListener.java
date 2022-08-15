6d0d070094d5280b4a5727f727ce40305289cde4
junjun2527 <xqwtxon@hotmail.com>
2022-08-15 12:58:46 +0800

    Fix Errors

--- src/main/PrideMC/SurvivalCore/EventListener.java
+++ src/main/PrideMC/SurvivalCore/EventListener.java
@@ -4,12 +4,21 @@ import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerJoinEvent;
 import cn.nukkit.event.player.PlayerQuitEvent;
 import cn.nukkit.event.block.BlockBreakEvent;
+import cn.nukkit.event.block.BlockPlaceEvent;
+import cn.nukkit.event.player.PlayerDropItemEvent;
+import cn.nukkit.event.entity.EntityDamageEvent;
 import cn.nukkit.Server;
 import cn.nukkit.Player;
 
 import PrideMC.SurvivalCore.Main;
 
 public class EventListener implements Listener{
+     
+     private Main plugin;
+     
+     public EventListener(Main plugin){
+          this.plugin = plugin;
+     }
      
      public void onJoin(PlayerJoinEvent event){
           event.setJoinMessage("");
@@ -20,25 +29,25 @@ public class EventListener implements Listener{
      }
      
      public void onBreak(BlockBreakEvent event){
-          if (event.getPlayer().getWorld().getFolderName() === "lobby"){
+          if (event.getPlayer().getLevel().getFolderName() == "lobby"){
                event.setCancelled(true);
           }
      }
      
      public void onPlace(BlockPlaceEvent event){
-          if (event.getPlayer().getWorld().getFolderName() === "lobby"){
+          if (event.getPlayer().getLevel().getFolderName() == "lobby"){
                event.setCancelled(true);
           }
      }
      
      public void onDrop(PlayerDropItemEvent event){
-          if (event.getPlayer().getWorld().getFolderName() === "lobby"){
+          if (event.getPlayer().getLevel().getFolderName() == "lobby"){
                event.setCancelled(true);
           }
      }
      
      public void onEntityDamage(EntityDamageEvent event){
-          if (event.getEntity().getWorld().getFolderName() == "lobby"){
+          if (event.getEntity().getLevel().getFolderName() == "lobby"){
                event.setCancelled(true);
           }
      }
--- src/main/PrideMC/SurvivalCore/Main.java
+++ src/main/PrideMC/SurvivalCore/Main.java
@@ -5,8 +5,8 @@ import cn.nukkit.utils.TextFormat;
 import cn.nukkit.utils.Config;
 
 import PrideMC.SurvivalCore.EventListener;
-import PrideMC.SurvivalCore.tasks.BroadcastTask;
-import PrideMC.SurvivalCore.tasks.MotdTask;
+//import PrideMC.SurvivalCore.tasks.BroadcastTask;
+//import PrideMC.SurvivalCore.tasks.MotdTask;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,8 +21,8 @@ public class Main extends PluginBase {
      
      @Override
      public void onEnable(){
-          this.getScheduler().scheduleRepeatingTask(new BroadcastTask(this), this);
-          this.getScheduler().scheduleRepeatingTask(new MotdTask(this), this);
+          //this.getScheduler().scheduleRepeatingTask(new BroadcastTask(this), this);
+          //this.getScheduler().scheduleRepeatingTask(new MotdTask(this), this);
           
           this.getServer().getPluginManager().registerEvents(new EventListener(this), this);
      }
