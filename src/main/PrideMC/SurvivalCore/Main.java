59f4c9a243d9b3fb713f7e30037a4d62d78eee0f
junjun2527 <xqwtxon@hotmail.com>
2022-08-15 18:24:27 +0800

    Hmm Tasks

--- src/main/PrideMC/SurvivalCore/Main.java
+++ src/main/PrideMC/SurvivalCore/Main.java
@@ -5,8 +5,8 @@ import cn.nukkit.utils.TextFormat;
 import cn.nukkit.utils.Config;
 
 import PrideMC.SurvivalCore.EventListener;
-//import PrideMC.SurvivalCore.tasks.BroadcastTask;
-//import PrideMC.SurvivalCore.tasks.MotdTask;
+import PrideMC.SurvivalCore.tasks.BroadcastTask;
+import PrideMC.SurvivalCore.tasks.MotdTask;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,8 +21,8 @@ public class Main extends PluginBase {
      
      @Override
      public void onEnable(){
-          //this.getScheduler().scheduleRepeatingTask(new BroadcastTask(this), this);
-          //this.getScheduler().scheduleRepeatingTask(new MotdTask(this), this);
+          this.getScheduler().scheduleRepeatingTask(new BroadcastTask(this), 20*60);
+          //this.getScheduler().scheduleRepeatingTask(new MotdTask(this), 20*60);
           
           this.getServer().getPluginManager().registerEvents(new EventListener(this), this);
      }
---
+++ src/main/PrideMC/SurvivalCore/tasks/BroadcastTask.java
@@ -0,0 +1,26 @@
+package PrideMC.SurvivalCore.tasks.BroadcastTask;
+
+import cn.nukkit.scheduler.Task;
+import cn.nukkit.Server;
+import cn.nukkit.utils.TextFormat;
+
+import java.util.Random;
+
+public class BroadcastTask extends Task {
+     
+     public BroadcastTask(Main plugin) {
+        super(plugin);
+    }
+     
+     private String[] messages = {
+          TextFormat.YELLOW + "PrideMC" + TextFormat.RESET + TextFormat.BOLD + TextFormat.GRAY + " » " + TextFormat.RESET + TextFormat.RED + "Thanks for playing in our server!",
+          TextFormat.YELLOW + "PrideMC" + TextFormat.RESET + TextFormat.BOLD + TextFormat.GRAY + " » " + TextFormat.RESET + TextFormat.GREEN + "Do you found a bug? Report us by using /report",
+          TextFormat.YELLOW + "PrideMC" + TextFormat.RESET + TextFormat.BOLD + TextFormat.GRAY + " » " + TextFormat.RESET + TextFormat.GOLD + "Join in our discord server to get latest updates from us!"
+     };
+     
+     @Override
+     public void onRun(int currentTick){
+          int random = new Random().nextInt(messages.length);
+          this.getPlugin().getServer().broadcastMessage(messages[random]);
+     }
+}
